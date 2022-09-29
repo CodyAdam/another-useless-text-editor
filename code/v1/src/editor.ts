626d6c821c50320a5cf6076a243cf25f2d0e5e3a
CodyAdam <cody.ad4@gmail.com>
2022-09-29 19:59:37 +0200

    feat: finished editor v1

--- code/v1/src/editor.ts
+++ code/v1/src/editor.ts
@@ -6,23 +6,65 @@ export class Editor {
     this.content = [];
   }
   deleteBetween(start: Position, end: Position): void {
-    // TODO
+    const allLinesIndexes = range(start.getLine(), end.getLine() + 1);
+    if (allLinesIndexes.length === 1) {
+        const line = this.content[start.getLine()];
+        this.content[start.getLine()] = getBefore(line, start.getCol()) + getAfter(line, end.getCol());
+    }
+    else {
+        const firstPart = getBefore(this.content[start.getLine()], start.getCol());
+        const lastPart = getAfter(this.content[end.getLine()], end.getCol());
+        this.content.splice( start.getLine(), allLinesIndexes.length, firstPart + lastPart);
+    }
   }
   deleteBefore(position: Position): void {
-    // TODO
+    const line = this.content[position.getLine()];
+        const x = position.getCol();
+        this.content[position.getLine()] = getBefore(line, x - 1) + getAfter(line, x);
   }
   deleteAfter(position: Position): void {
-    // TODO
+    const line = this.content[position.getLine()];
+    const x = position.getCol();
+    this.content[position.getLine()] = getBefore(line, x) + getAfter(line, x + 1);
   }
   getBetween(start: Position, end: Position): string {
-
     const allLines = this.content.slice(start.getLine(), end.getLine() + 1);
-
-    return "";
+    if (allLines.length === 1) {
+      return getBetween(allLines[0], start.getCol(), end.getCol());
+    } else {
+      allLines[0] = getAfter(allLines[0], start.getCol());
+      allLines[allLines.length - 1] = getBefore(allLines[allLines.length - 1], end.getCol());
+      return allLines.join('\n');
+    }
   }
   insertAt(pos: Position, text: string): void {
     // TODO
   }
+}
 
-  private
+
+// Helper function 
+
+function range(start: number, end: number): number[] {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+function getBetween(string: string, indexStart: number, indexEnd: number): string {
+  return string.substring(indexStart, indexEnd);
+}
+
+function getAfter(string: string, index: number): string {
+  if (index < 0) 
+    return string;
+  return string.slice(index);
+}
+
+function getBefore(string: string, index: number): string {
+  if (index < 0) 
+    return "";
+  return string.slice(0, index);
 }
