b3ba0f50adfbd1e3d061af9aceda1783d6d97cd4
CodyAdam <cody.ad4@gmail.com>
2022-10-21 12:37:56 +0200

    fix: clamp

--- code/src/v2/commands.ts
+++ code/src/v2/commands.ts
@@ -34,7 +34,7 @@ export class WriteCommand extends UndoableCommand {
   private beforeCur: Cursor | null;
   private afterPos: Position | null;
   constructor(cur: Cursor, edit: Editor, text: string) {
-    super(`${text.length > 1 ? "Paste" : "Write"} "${text.replace(/\n/g, "\\n")}"`);
+    super(`${text.length > 1 ? 'Paste' : 'Write'} "${text.replace(/\n/g, '\\n')}"`);
     this.cur = cur;
     this.edit = edit;
     this.text = text;
@@ -44,13 +44,11 @@ export class WriteCommand extends UndoableCommand {
   }
   execute(): void {
     // INIT CURSOR POS
-    if (this.beforeCur)
-      this.cur.copy(this.beforeCur);
-    else
-      this.beforeCur = Cursor.from(this.cur);
+    if (this.beforeCur) this.cur.copy(this.beforeCur);
+    else this.beforeCur = Cursor.from(this.cur);
 
     // ORDER START END
-    let start = this.cur.getStart()
+    let start = this.cur.getStart();
     let end = this.cur.getEnd();
     if (end.isBefore(start)) {
       const tmp = start;
@@ -67,26 +65,28 @@ export class WriteCommand extends UndoableCommand {
     }
 
     // WHRITE
-    const textByLines = this.text.split("\n");
+    const textByLines = this.text.split('\n');
     this.edit.insertAt(this.cur.getStart(), this.text);
 
     //MOVE CURSOR
     if (textByLines.length == 1)
-      this.cur.setStart(new Position(start.getLine(), start.getCol() + textByLines[0].length))
+      this.cur.setStart(new Position(start.getLine(), start.getCol() + textByLines[0].length));
     else
-      this.cur.setStart(new Position(start.getLine() + textByLines.length - 1, textByLines[textByLines.length - 1].length))
-    this.cur.setEnd(this.cur.getStart())
-    this.afterPos = this.cur.getStart()
+      this.cur.setStart(
+        new Position(start.getLine() + textByLines.length - 1, textByLines[textByLines.length - 1].length),
+      );
+    this.cur.setEnd(this.cur.getStart());
+    this.afterPos = this.cur.getStart();
   }
 
   undo(): void {
     if (!this.beforeCur || !this.afterPos) {
-      console.error("undo called before execute")
-      return
+      console.error('undo called before execute');
+      return;
     }
 
     // ORDER START END
-    let start = this.beforeCur.getStart()
+    let start = this.beforeCur.getStart();
     let end = this.beforeCur.getEnd();
     if (end.isBefore(start)) {
       const tmp = start;
@@ -96,8 +96,7 @@ export class WriteCommand extends UndoableCommand {
 
     // DELETE AND WRITE
     this.edit.deleteBetween(start, this.afterPos);
-    if (this.deletedText)
-      this.edit.insertAt(start, this.deletedText);
+    if (this.deletedText) this.edit.insertAt(start, this.deletedText);
 
     // SET PREVIOUS CURSOR
     this.cur.copy(this.beforeCur);
@@ -120,13 +119,11 @@ export class BackspaceCommand extends UndoableCommand {
   }
   execute(): void {
     // INIT CURSOR POS
-    if (this.beforeCur)
-      this.cur.copy(this.beforeCur);
-    else
-      this.beforeCur = Cursor.from(this.cur);
+    if (this.beforeCur) this.cur.copy(this.beforeCur);
+    else this.beforeCur = Cursor.from(this.cur);
 
     // ORDER START END
-    let start = this.cur.getStart()
+    let start = this.cur.getStart();
     let end = this.cur.getEnd();
     if (end.isBefore(start)) {
       const tmp = start;
@@ -138,36 +135,32 @@ export class BackspaceCommand extends UndoableCommand {
     if (!this.cur.isSelection()) {
       const x = start.getCol();
       const y = start.getLine();
-      if (x > 0)
-        start = new Position(start.getLine(), start.getCol() - 1);
-      else if (y > 0)
-        start = this.edit.getEndLinePos(y - 1);
+      if (x > 0) start = new Position(start.getLine(), start.getCol() - 1);
+      else if (y > 0) start = this.edit.getEndLinePos(y - 1);
     }
     this.deletedText = this.edit.getBetween(start, end);
-    this.setName(`Delete "${this.deletedText.replace(/\n/g, "\\n")}"`);
+    this.setName(`Delete "${this.deletedText.replace(/\n/g, '\\n')}"`);
     this.edit.deleteBetween(start, end);
 
     // UPDATE CURSOR
     this.cur.setStart(start);
     this.cur.setEnd(start);
-    this.afterPos = this.cur.getStart()
+    this.afterPos = this.cur.getStart();
   }
 
   undo(): void {
     if (!this.beforeCur || !this.afterPos) {
-      console.error("undo called before execute")
-      return
+      console.error('undo called before execute');
+      return;
     }
 
     // DELETE AND WRITE
-    if (this.deletedText !== null)
-      this.edit.insertAt(this.afterPos, this.deletedText);
+    if (this.deletedText !== null) this.edit.insertAt(this.afterPos, this.deletedText);
 
     // SET PREVIOUS CURSOR
     this.cur.copy(this.beforeCur);
   }
 }
-
 
 export class DeleteCommand extends UndoableCommand {
   private cur: Cursor;
@@ -185,13 +178,11 @@ export class DeleteCommand extends UndoableCommand {
   }
   execute(): void {
     // INIT CURSOR POS
-    if (this.beforeCur)
-      this.cur.copy(this.beforeCur);
-    else
-      this.beforeCur = Cursor.from(this.cur);
+    if (this.beforeCur) this.cur.copy(this.beforeCur);
+    else this.beforeCur = Cursor.from(this.cur);
 
     // ORDER START END
-    let start = this.cur.getStart()
+    let start = this.cur.getStart();
     let end = this.cur.getEnd();
     if (end.isBefore(start)) {
       const tmp = start;
@@ -203,36 +194,32 @@ export class DeleteCommand extends UndoableCommand {
     if (!this.cur.isSelection()) {
       const x = start.getCol();
       const y = start.getLine();
-      if (x < this.edit.getEndLinePos(y).getCol())
-        end = new Position(start.getLine(), start.getCol() + 1);
-      else if (y < this.edit.getLineCount() - 1)
-        end = this.edit.getStartLinePos(y + 1);
+      if (x < this.edit.getEndLinePos(y).getCol()) end = new Position(start.getLine(), start.getCol() + 1);
+      else if (y < this.edit.getLineCount() - 1) end = this.edit.getStartLinePos(y + 1);
     }
     this.deletedText = this.edit.getBetween(start, end);
-    this.setName(`Delete "${this.deletedText.replace(/\n/g, "\\n")}"`);
+    this.setName(`Delete "${this.deletedText.replace(/\n/g, '\\n')}"`);
     this.edit.deleteBetween(start, end);
 
     // UPDATE CURSOR
     this.cur.setStart(start);
     this.cur.setEnd(start);
-    this.afterPos = this.cur.getStart()
+    this.afterPos = this.cur.getStart();
   }
 
   undo(): void {
     if (!this.beforeCur || !this.afterPos) {
-      console.error("undo called before execute")
-      return
+      console.error('undo called before execute');
+      return;
     }
 
     // DELETE AND WRITE
-    if (this.deletedText !== null)
-      this.edit.insertAt(this.afterPos, this.deletedText);
+    if (this.deletedText !== null) this.edit.insertAt(this.afterPos, this.deletedText);
 
     // SET PREVIOUS CURSOR
     this.cur.copy(this.beforeCur);
   }
 }
-
 
 export class CopyCommand extends Command {
   private cur: Cursor;
@@ -246,7 +233,7 @@ export class CopyCommand extends Command {
   }
   execute(): void {
     const newClip = this.edit.getBetween(this.cur.getStart(), this.cur.getEnd());
-    this.setName(`Copy "${newClip}"`)
+    this.setName(`Copy "${newClip}"`);
     this.app.setClipboard(newClip);
   }
 }
@@ -254,42 +241,48 @@ export class CopyCommand extends Command {
 export class MoveCursorCommand extends Command {
   private cur: Cursor;
   private pos: Position;
-  constructor(cur: Cursor, pos: Position) {
+  private edit: Editor;
+  constructor(cur: Cursor, pos: Position, edit: Editor) {
     super('Move cursor');
     this.cur = cur;
     this.pos = pos;
+    this.edit = edit;
   }
   execute(): void {
-    this.cur.setStart(this.pos);
-    this.cur.setEnd(this.pos);
-    this.setName(`Move to ${this.pos.toString()}`)
+    this.cur.setStart(this.edit.clampedPosition(this.pos));
+    this.cur.setEnd(this.edit.clampedPosition(this.pos));
+    this.setName(`Move to ${this.pos.toString()}`);
   }
 }
 
 export class MoveStartCursorCommand extends Command {
   private pos: Position;
   private cur: Cursor;
-  constructor(cur: Cursor, pos: Position) {
+  private edit: Editor;
+  constructor(cur: Cursor, pos: Position, edit: Editor) {
     super('Move start cursor');
     this.cur = cur;
     this.pos = pos;
+    this.edit = edit;
   }
   execute(): void {
-    this.cur.setStart(this.pos);
-    this.setName(`Move to ${this.pos.toString()}`)
+    this.cur.setStart(this.edit.clampedPosition(this.pos));
+    this.setName(`Move to ${this.pos.toString()}`);
   }
 }
 
 export class MoveEndCursorCommand extends Command {
   private pos: Position;
   private cur: Cursor;
-  constructor(cur: Cursor, pos: Position) {
+  private edit: Editor;
+  constructor(cur: Cursor, pos: Position, edit: Editor) {
     super('Move end cursor');
     this.cur = cur;
     this.pos = pos;
+    this.edit = edit;
   }
   execute(): void {
-    this.cur.setEnd(this.pos);
-    this.setName(`Move end to ${this.pos.toString()}`)
+    this.cur.setEnd(this.edit.clampedPosition(this.pos));
+    this.setName(`Move end to ${this.pos.toString()}`);
   }
 }
